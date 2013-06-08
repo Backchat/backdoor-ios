2ef51aef0af6354be295d809f704fe66b4f272f7
Dan <coder@capricalabs.com>
2013-06-08 22:24:12 +0200

    Transition to com.backdoor.Backdoor

--- YouTellMobile/YouTellMobile/Main/YTConfig.h
+++ YouTellMobile/YouTellMobile/Main/YTConfig.h
@@ -5,7 +5,7 @@
 //  Copyright (c) 2013 Backdoor LLC. All rights reserved.
 //
 
-#define CONFIG_APPLE_ID @"620421403"
+#define CONFIG_APPLE_ID @"659951602"
 //#define CONFIG_URL @"https://capricalabs.com:7564/"
 #define CONFIG_URL @"https://backdoor-api.herokuapp.com/"
 #define CONFIG_MODEL @"YTModel16"
