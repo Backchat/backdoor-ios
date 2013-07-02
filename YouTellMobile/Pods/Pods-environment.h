9806921d830982eb49903c3116ff0a4008ead42a
Dan <coder@capricalabs.com>
2013-07-02 10:52:35 +0200

    Update pods. Add mixpanel.

--- YouTellMobile/Pods/Pods-environment.h
+++ YouTellMobile/Pods/Pods-environment.h
@@ -42,6 +42,12 @@
 #define COCOAPODS_VERSION_MINOR_HockeySDK 0
 #define COCOAPODS_VERSION_PATCH_HockeySDK 0
 
+// Mixpanel
+#define COCOAPODS_POD_AVAILABLE_Mixpanel
+#define COCOAPODS_VERSION_MAJOR_Mixpanel 2
+#define COCOAPODS_VERSION_MINOR_Mixpanel 0
+#define COCOAPODS_VERSION_PATCH_Mixpanel 0
+
 // NoticeView
 #define COCOAPODS_POD_AVAILABLE_NoticeView
 #define COCOAPODS_VERSION_MAJOR_NoticeView 3
