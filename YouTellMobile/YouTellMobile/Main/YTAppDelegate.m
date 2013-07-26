473292cb0748c1f56e189d152f6d7552826d0212
dan <coder@capricalabs.com>
2013-07-26 12:14:52 -0700

    enabled Flurry crash reporting

--- YouTellMobile/YouTellMobile/Main/YTAppDelegate.m
+++ YouTellMobile/YouTellMobile/Main/YTAppDelegate.m
@@ -112,6 +112,8 @@ void uncaughtExceptionHandler(NSException *exception)
         [Flurry setDebugLogEnabled:YES];
         [Flurry setEventLoggingEnabled:YES];
     }
+    
+    [Flurry setCrashReportingEnabled:YES];
     [Flurry startSession:CONFIG_FLURRY_APP_TOKEN];
     
     [Mixpanel sharedInstanceWithToken:CONFIG_MIXPANEL_TOKEN];
